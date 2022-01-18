90481706f3ac50d7c16af2a93007b2d56426683a
Chuck Remes <git@chuckremes.com>
2022-01-18 09:23:03 -0600

    pass through underlying price so options calcs are accurate for SPREADS

--- lib/ttk/platform/rspec/helper.rb
+++ lib/ttk/platform/rspec/helper.rb
@@ -9,12 +9,12 @@ module Helper
   # By default, strikes are 10 points apart. The +side+ makes it either
   # a bear spread; switching to :short makes it a bull spread.
   #
-  def make_put_vertical_position(side: :long, quantity: 1, strike: 100)
+  def make_put_vertical_position(side: :long, quantity: 1, strike: 100, underlying_last: strike - 1)
     legs = make_put_vertical_position_legs(side: side, quantity: quantity, strike: strike)
     TTK::Platform::Wrappers::Combo::Vertical.new(legs)
   end
 
-  def make_put_vertical_position_legs(side: :long, quantity: 1, strike: 100)
+  def make_put_vertical_position_legs(side: :long, quantity: 1, strike: 100, underlying_last: strike - 1)
     side1, side2 = side == :long ? [:long, :short] : [:short, :long]
 
     legs = [make_option_leg(callput: :put, side: side2, strike: strike - 10, filled_quantity: quantity, leg_id: 1),
@@ -22,16 +22,18 @@ module Helper
     TTK::Containers::Legs::Position::Example.new(legs: legs)
   end
 
-  def make_call_vertical_position(side: :long, quantity: 1, strike: 100)
+  def make_call_vertical_position(side: :long, quantity: 1, strike: 100, underlying_last: strike - 1)
     legs = make_call_vertical_position_legs(side: side, quantity: quantity, strike: strike)
     TTK::Platform::Wrappers::Combo::Vertical.new(legs)
   end
 
-  def make_call_vertical_position_legs(side: :long, quantity: 1, strike: 100)
+  def make_call_vertical_position_legs(side: :long, quantity: 1, strike: 100, underlying_last: strike - 1)
     side1, side2 = side == :long ? [:long, :short] : [:short, :long]
 
-    legs = [make_option_leg(callput: :call, side: side1, strike: strike - 10, filled_quantity: quantity, leg_id: 1),
-            make_option_leg(callput: :call, side: side2, strike: strike,  filled_quantity: quantity, leg_id: 2)]
+    legs = [make_option_leg(callput: :call, side: side1, strike: strike - 10, filled_quantity: quantity, leg_id: 1,
+      underlying_last: underlying_last),
+            make_option_leg(callput: :call, side: side2, strike: strike,  filled_quantity: quantity, leg_id: 2,
+              underlying_last: underlying_last)]
     TTK::Containers::Legs::Position::Example.new(legs: legs)
   end
 
