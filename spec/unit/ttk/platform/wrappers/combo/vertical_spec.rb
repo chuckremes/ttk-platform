766355577f9d1b5666d5fc82acc6891b80a7c12a
Chuck Remes <git@chuckremes.com>
2022-01-18 09:22:20 -0600

    test delta and theta to confirm short/long spreads return good numbers

--- spec/unit/ttk/platform/wrappers/combo/vertical_spec.rb
+++ spec/unit/ttk/platform/wrappers/combo/vertical_spec.rb
@@ -3,10 +3,11 @@ RSpec.describe TTK::Platform::Wrappers::Combo::Vertical do
 
   let(:spread) { TTK::Platform::Wrappers::Position.new(container) }
   let(:symbol) { "SPY" }
+  let(:underlying_last) { 97.22 }
 
   context "with puts" do
     context "given a bull put combo" do
-      let(:container) { make_put_vertical_position(side: :short) }
+      let(:container) { make_put_vertical_position(side: :short, underlying_last: underlying_last) }
       let(:strike1) { spread.legs[0].strike }
       let(:strike2) { spread.legs[1].strike }
 
@@ -59,10 +60,24 @@ RSpec.describe TTK::Platform::Wrappers::Combo::Vertical do
           expect(long?).to be false
         end
       end
+
+      describe "#delta" do
+        subject(:delta) { vertical.delta }
+        it "returns a positive number" do
+          expect(delta).to be_positive
+        end
+      end
+
+      describe "#theta" do
+        subject(:theta) { vertical.theta }
+        it "returns a positive number" do
+          expect(theta).to be_positive
+        end
+      end
     end
 
     context "given a bear put combo" do
-      let(:container) { make_put_vertical_position(side: :long) }
+      let(:container) { make_put_vertical_position(side: :long, underlying_last: underlying_last) }
       let(:strike1) { spread.legs[0].strike }
       let(:strike2) { spread.legs[1].strike }
 
@@ -115,12 +130,26 @@ RSpec.describe TTK::Platform::Wrappers::Combo::Vertical do
           expect(long?).to be true
         end
       end
+
+      describe "#delta" do
+        subject(:delta) { vertical.delta }
+        it "returns a negative number" do
+          expect(delta).to be_negative
+        end
+      end
+
+      describe "#theta" do
+        subject(:theta) { vertical.theta }
+        it "returns a negative number" do
+          expect(theta).to be_negative
+        end
+      end
     end
   end
 
   context "with calls" do
     context "given a bear call combo" do
-      let(:container) { make_call_vertical_position(side: :short) }
+      let(:container) { make_call_vertical_position(side: :short, underlying_last: underlying_last) }
       let(:strike1) { spread.legs[0].strike }
       let(:strike2) { spread.legs[1].strike }
 
@@ -173,10 +202,24 @@ RSpec.describe TTK::Platform::Wrappers::Combo::Vertical do
           expect(long?).to be false
         end
       end
+
+      describe "#delta" do
+        subject(:delta) { vertical.delta }
+        it "returns a negative number" do
+          expect(delta).to be_negative
+        end
+      end
+
+      describe "#theta" do
+        subject(:theta) { vertical.theta }
+        it "returns a positive number" do
+          expect(theta).to be_positive
+        end
+      end
     end
 
     context "given a bull call combo" do
-      let(:container) { make_call_vertical_position(side: :long) }
+      let(:container) { make_call_vertical_position(side: :long, underlying_last: underlying_last) }
       let(:strike1) { spread.legs[0].strike }
       let(:strike2) { spread.legs[1].strike }
 
@@ -227,6 +270,20 @@ RSpec.describe TTK::Platform::Wrappers::Combo::Vertical do
         subject(:long?) { vertical.long? }
         it "returns true" do
           expect(long?).to be true
+        end
+      end
+
+      describe "#delta" do
+        subject(:delta) { vertical.delta }
+        it "returns a positive number" do
+          expect(delta).to be_positive
+        end
+      end
+
+      describe "#theta" do
+        subject(:theta) { vertical.theta }
+        it "returns a negative number" do
+          expect(theta).to be_negative
         end
       end
     end
